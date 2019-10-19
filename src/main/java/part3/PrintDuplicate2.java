125a179aa9099bb92d8e6677771d8b872d7f24c5
xyh930929@163.com <xyh930929@163.com>
2019-10-19 13:27:44 +0800

    第三题 第二种解法

---
+++ src/main/java/part3/PrintDuplicate2.java
@@ -0,0 +1,14 @@
+package part3;
+
+public class PrintDuplicate2 {
+
+    public static void main(String[] args) {
+        HashTable table = new HashTable();
+        table.add(1);
+        table.add(11);
+        table.add(101);
+
+        table.print(1);
+    }
+
+}

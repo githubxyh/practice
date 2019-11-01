e278977c6df7ffec0d75876bf321c6adaa372b07
xuyh <xyh930929@163.com>
2019-11-01 14:47:22 +0800

    计数排序

--- src/main/java/sort/CountSort.java
+++ src/main/java/sort/CountSort.java
@@ -1,7 +1,8 @@
 package sort;
 
 /**
- * 计数排序
+ * 计数排序：创建一个长度为（最大值-最小值）长度的数组newArr，遍历要排序数组，将每个元素都减去最小值，
+ * 得到的值作为索引，索引到newArr位置，将该位置加1。最后遍历新数组newArr，将newArr中不为0的索引位置加上最小值就是最终的值。且按照这个顺序取值后就是一个有序数组。
  */
 public class CountSort {
     public static void main(String[] args) {
@@ -14,18 +15,11 @@ public class CountSort {
         if(arr.length == 0){
             return;
         }
-
         int min = arr[0];
         int max = 0;
-
         for(int a : arr){
-            if(min >= a){
-                min = a;
-            }
-
-            if(max < a){
-                max = a;
-            }
+            if(min >= a)min = a;
+            if(max < a) max = a;
         }
 
         int[] newInt = new int[(max - min)+1];
